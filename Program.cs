a84fe7a96c4601994605c9bbfd9710b1ec696238
s24242 <s24242@pjwstk.edu.pl>
2024-03-26 21:43:17 +0100

    Dodano nowa metode CalculateAverage(int[] numbers)

--- Program.cs
+++ Program.cs
@@ -31,5 +31,21 @@ namespace InitialProject
             Console.ReadKey();
             Console.WriteLine("Zakończono działanie programu.");
         }
+        
+        public static double CalculateAverage(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be null or empty");
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return (double)sum / numbers.Length;
+        }
     }
 }
