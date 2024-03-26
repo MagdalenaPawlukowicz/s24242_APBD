436c554fe31977e7efac383dbab4b65def34540f
s24242 <s24242@pjwstk.edu.pl>
2024-03-26 21:38:47 +0100

    Modyfikacja 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
-﻿namespace InitialProject
+﻿using System;
+
+namespace InitialProject
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            Console.WriteLine("Witaj!");
+            Console.WriteLine("Podaj swoje imię:");
+            
+            // Pobranie imienia od użytkownika
+            string name = Console.ReadLine();
+
+            // Wyświetlenie powitania
+            Console.WriteLine("Podaj swoje nazwisko:");
+            
+            // Pobranie imienia od użytkownika
+            string surname = Console.ReadLine();
+
+            // Wyświetlenie powitania
+            Console.WriteLine($"Witaj, {name} {surname}!");
+
+            // Prosta operacja matematyczna
+            int a = 5;
+            int b = 3;
+            int sum = a + b;
+            Console.WriteLine($"Suma {a} i {b} wynosi: {sum}");
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
+            Console.WriteLine("Zakończono działanie programu.");
         }
     }
 }
