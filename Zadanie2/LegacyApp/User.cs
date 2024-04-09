bcb4811ebff0724c603b26ac5708095893ba3d01
s24242 <s24242@pjwstk.edu.pl>
2024-04-09 19:37:28 +0200

    Dodano nowy konstruktor User(client, dateOfBirth, emailAddress, firstName, lastName)

--- Zadanie2/LegacyApp/User.cs
+++ Zadanie2/LegacyApp/User.cs
@@ -8,6 +8,15 @@ namespace LegacyApp
         {
         }
 
+        public User(object client, DateTime dateOfBirth, string emailAddress, string firstName, string lastName)
+        {
+            Client = client;
+            DateOfBirth = dateOfBirth;
+            EmailAddress = emailAddress;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
         public object Client { get; internal set; }
         public DateTime DateOfBirth { get; internal set; }
         public string EmailAddress { get; internal set; }
