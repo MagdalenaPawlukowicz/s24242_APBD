00b3da886928e3919d4a37f0f3c86412ff77a81f
s24242 <s24242@pjwstk.edu.pl>
2024-04-16 20:31:27 +0200

    Dodanie pól i adnotacji w klasie Animals

---
+++ Zadanie3/Zadanie3_s24242/Models/Animals.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zadanie3_s24242.Models;
+
+public class Animals
+{
+    [Required]
+    public int IdAnimal { get; set; }
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; }
+    [StringLength(200)]
+    public string Description { get; set; }
+    [Required]
+    [StringLength(200)]
+    public string Category { get; set; }
+    [Required]
+    [StringLength(200)]
+    public string Area { get; set; }
+}
