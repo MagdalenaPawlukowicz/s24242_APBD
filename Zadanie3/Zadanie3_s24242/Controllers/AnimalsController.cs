955f4fd87db5f9c1f7e53abe1c4f0de7bca5c164
s24242 <s24242@pjwstk.edu.pl>
2024-04-20 19:32:58 +0200

    Dodanie metody AddAnimals - HttpGet

--- Zadanie3/Zadanie3_s24242/Controllers/AnimalsController.cs
+++ Zadanie3/Zadanie3_s24242/Controllers/AnimalsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Zadanie3_s24242.Models;
+using Zadanie3_s24242.Repositories;
 
 namespace Zadanie3_s24242.Controllers;
 
@@ -6,5 +8,36 @@ namespace Zadanie3_s24242.Controllers;
 [ApiController]
 public class AnimalsController : ControllerBase
 {
+    private IAnimalsRepository _animalsRepository;
     
+    public AnimalsController(IAnimalsRepository animalsRepository)
+    {
+        _animalsRepository = animalsRepository;
+    }
+    
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> GetAnimals(string orderBy = "name")
+    {
+        var animals = _animalsRepository.GetRepoAnimals().ToList();
+
+        switch (orderBy.ToLower())
+        {
+            case "description":
+                animals.Sort((a1, a2) => string.Compare(a1.Description, a2.Description, StringComparison.OrdinalIgnoreCase));
+                break;
+            case "category":
+                animals.Sort((a1, a2) => string.Compare(a1.Category, a2.Category, StringComparison.OrdinalIgnoreCase));
+                break;
+            case "area":
+                animals.Sort((a1, a2) => string.Compare(a1.Area, a2.Area, StringComparison.OrdinalIgnoreCase));
+                break;
+            default:
+                animals.Sort((a1, a2) => string.Compare(a1.Name, a2.Name, StringComparison.OrdinalIgnoreCase));
+                break;
+        }
+
+        return animals;
+    }
+
 }
