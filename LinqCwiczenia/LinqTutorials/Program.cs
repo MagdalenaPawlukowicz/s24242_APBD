7cd28c372d4f0b0243334ce630c3a5d37da11a9a
s24242 <s24242@pjwstk.edu.pl>
2024-05-24 22:41:44 +0200

    Zadanie dodatkowe

---
+++ LinqCwiczenia/LinqTutorials/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = LinqTasks.Task1();
+            var t2 = LinqTasks.Task2();
+            var t3 = LinqTasks.Task3();
+            var t4 = LinqTasks.Task4();
+            var t5 = LinqTasks.Task5();
+            var t6 = LinqTasks.Task6();
+            var t7 = LinqTasks.Task7();
+            var t8 = LinqTasks.Task8();
+            var t9 = LinqTasks.Task9();
+            var t10 = LinqTasks.Task10();
+            var t11 = LinqTasks.Task11();
+            var t12 = LinqTasks.Task12();
+            int[] tab = {1,1,10,10,9,9,9,9,1,1,1,1,8};
+            var t13 = LinqTasks.Task13(tab);
+            var t14 = LinqTasks.Task14();
+            var t15 = LinqTasks.Task15();
+            var t16 = LinqTasks.Task16();
+
+        }
+    }
+}
