2bcff9239272adb68dee78b3cb228cae4eae31dd
s24242 <s24242@pjwstk.edu.pl>
2024-06-08 18:41:49 +0200

    Metoda post poprawiona

--- Kolokwium_s24242/Kolokwium_s24242/Controllers/ShopController.cs
+++ Kolokwium_s24242/Kolokwium_s24242/Controllers/ShopController.cs
@@ -48,6 +48,79 @@ public class ShopController : ControllerBase
         return Ok(clientData);
     }
 
+[HttpPost]
+public async Task<IActionResult> CreatePayment([FromBody] PaymentRequestDto request)
+{
+    
+    var client = await _context.Clients.FindAsync(request.IdClient);
+    if (client == null)
+    {
+        return NotFound("Klient nie istnieje");
+    }
+
+    var subscription = await _context.Subscriptions.FindAsync(request.IdSubscription);
+    if (subscription == null)
+    {
+        return NotFound("Subskrypcja nie istnieje.");
+    }
+
+    
+    if (subscription.EndTime < DateTime.UtcNow)
+    {
+        return BadRequest("Subskrypcja nie aktywna.");
+    }
+
+    
+    var lastSale = await _context.Sales
+        .Where(s => s.IdClient == request.IdClient && s.IdSubscription == request.IdSubscription)
+        .OrderByDescending(s => s.CreatedAt)
+        .FirstOrDefaultAsync();
+
+    if (lastSale == null)
+    {
+        return BadRequest("brak rekordu sprzedaży dla tej subskrypcjii");
+    }
+    
+    var nextPaymentStartDate = lastSale.CreatedAt.AddMonths(subscription.RenewalPeriod);
+    
+    var existingPayment = await _context.Payments
+        .Where(p => p.IdClient == request.IdClient && p.IdSubscription == request.IdSubscription && p.Date >= nextPaymentStartDate)
+        .FirstOrDefaultAsync();
+
+    if (existingPayment != null)
+    {
+        return BadRequest("Płatność za ten okres została  dokonana");
+    }
+    
+    decimal finalPaymentAmount = subscription.Price;
+
+    var highestDiscount = await _context.Discounts
+        .Where(d => d.IdSubscription == request.IdSubscription && d.DateFrom <= DateTime.UtcNow && d.DateTo >= DateTime.UtcNow)
+        .OrderByDescending(d => d.Value)
+        .FirstOrDefaultAsync();
+
+    if (highestDiscount != null)
+    {
+        finalPaymentAmount -= finalPaymentAmount * (highestDiscount.Value / 100.0m);
+    }
+    
+    if (request.Payment != finalPaymentAmount)
+    {
+        return BadRequest($"Nieprawidłowa płatność. Powinno być: {finalPaymentAmount}");
+    }
+    
+    var payment = new Payment
+    {
+        Date = DateTime.UtcNow,
+        IdClient = request.IdClient,
+        IdSubscription = request.IdSubscription
+    };
+
+    _context.Payments.Add(payment);
+    await _context.SaveChangesAsync();
+    
+    return Ok(new { IdPayment = payment.IdPayment });
+}
 }
 
 
