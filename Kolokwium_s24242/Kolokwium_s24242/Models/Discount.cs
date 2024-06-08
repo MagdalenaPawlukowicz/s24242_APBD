77d1700f2b7619eaf6d28c2ae987dacfa492f463
s24242 <s24242@pjwstk.edu.pl>
2024-06-08 18:17:21 +0200

    Dodano modele

---
+++ Kolokwium_s24242/Kolokwium_s24242/Models/Discount.cs
@@ -0,0 +1,13 @@
+﻿namespace Kolokwium_s24242.Models;
+
+public class Discount
+{
+    public int IdDiscount { get; set; }
+    public int Value { get; set; }
+    public int IdSubscription { get; set; }
+    public DateTime DateFrom { get; set; }
+
+    public DateTime DateTo { get; set; }
+
+    public Subscription Subscription { get; set; }
+}
---
+++ Kolokwium_s24242/Kolokwium_s24242/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Kolokwium_s24242.Models;
+
+public class Payment
+{
+    public int IdPayment { get; set; }
+    public DateTime Date { get; set; }
+    public int IdClient { get; set; }
+    public int IdSubscription { get; set; }
+    public Client Client { get; set; }
+    public Subscription Subscription { get; set; }
+}
